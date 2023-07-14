0131ee137d63996461af0a5a10dd7c18cc726780
Timothy Tandt <Timothy.tandt@outlook.be>
2023-07-14 18:35:19 +0200

    Feature: Auto Link drive

--- DriveLinker/ViewModels/MainViewModel.cs
+++ DriveLinker/ViewModels/MainViewModel.cs
@@ -5,29 +5,41 @@ public partial class MainViewModel : BaseViewModel
     
     private readonly IDriveService _driveService;
     private readonly IDummyService _dummyService;
+    private readonly ISettingsService _settingsService;
     private readonly ILinker _linker;
 
     public MainViewModel(
         IDriveService driveService,
         IDummyService dummyService,
+        ISettingsService settingsService,
         ILinker linker)
     {
         _driveService = driveService;
         _dummyService = dummyService;
+        _settingsService = settingsService;
         _linker = linker;
     }
+    
+    [ObservableProperty]
+    private bool _isNotAlreadyConnected = true;
 
-    [ObservableProperty] private ObservableCollection<Drive> _drives = new();
-
+    [ObservableProperty] 
+    private ObservableCollection<Drive> _drives = new();
 
     private void ChecksDriveConnection(Drive drive)
     {
         _linker.IsDriveConnected(drive);
     }
 
+    private async Task LinkDriveAsync(Drive drive)
+    {
+        await _linker.ConnectDriveAsync(drive);
+    }
+
     [RelayCommand]
     private async Task LoadDrivesAsync()
     {
+        var settings = await _settingsService.GetSettingsAsync();
         var drives = await _driveService.GetAllDrivesAsync();
 
         if (drives?.Count <= 0)
@@ -36,8 +48,13 @@ public partial class MainViewModel : BaseViewModel
         }
 
         Parallel.ForEach(drives, ChecksDriveConnection);
-
         Drives = new(drives);
+
+        if (settings.AutoLink && IsNotAlreadyConnected)
+        {
+            await LinkAllDrivesAsync();
+            IsNotAlreadyConnected = false;
+        }
     }
 
     [RelayCommand]
