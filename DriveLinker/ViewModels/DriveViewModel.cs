af504740cfbf0a38a99a461919ad2759e5f403f7
Timothy Tandt <Timothy.tandt@outlook.be>
2023-07-16 17:36:06 +0200

    Implemented update methods for DrivePage
    DrivePage is now able to update drives.

--- DriveLinker/ViewModels/DriveViewModel.cs
+++ DriveLinker/ViewModels/DriveViewModel.cs
@@ -3,18 +3,35 @@
 public partial class DriveViewModel : BaseViewModel, IQueryAttributable
 {
     private readonly IDriveService _driveService;
+    private readonly IAesEncryption _encryption;
 
-    public DriveViewModel(IDriveService driveService)
+    public DriveViewModel(
+        IDriveService driveService,
+        IAesEncryption encryption)
     {
         _driveService = driveService;
+        _encryption = encryption;
     }
 
     [ObservableProperty] 
     private Drive _drive;
 
-    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    public async void ApplyQueryAttributes(IDictionary<string, object> query)
     {
         Drive = query["Drive"] as Drive;
+        await DecryptInformationAsync();
+    }
+
+    private async Task DecryptInformationAsync()
+    {
+        Drive.Password = await _encryption.DecryptAsync(Drive.Password, Drive.Key, Drive.Iv);
+        Drive.UserName = await _encryption.DecryptAsync(Drive.UserName, Drive.Key, Drive.Iv);
+        Drive.IpAddress = await _encryption.DecryptAsync(Drive.IpAddress, Drive.Key, Drive.Iv);
+    }
+
+    private static async Task<string> PromptUserAsync(string title, string message)
+    {
+        return await Shell.Current.DisplayPromptAsync(title, message);
     }
 
     [RelayCommand]
@@ -40,8 +57,62 @@ public partial class DriveViewModel : BaseViewModel, IQueryAttributable
     }
 
     [RelayCommand]
-    private async Task LoadUpdatePageAsync()
+    private async Task UpdateLetterAsync()
     {
-        await Shell.Current.GoToAsync(nameof(UpdatePage), true);
+        string result = await PromptUserAsync("Edit Letter?", "What's your new letter?");
+
+        if (string.IsNullOrWhiteSpace(result) is false && Drive.Letter.Length == 1)
+        {
+            Drive.Letter = result;
+            await _driveService.UpdateDriveAsync(Drive);
+        }
+    }
+
+    [RelayCommand]
+    private async Task UpdateDriveNameAsync()
+    {
+        string result = await PromptUserAsync("Edit Drive Name?", "What's your new drive name?");
+
+        if (string.IsNullOrWhiteSpace(result) is false)
+        {
+            Drive.DriveName = result;
+            await _driveService.UpdateDriveAsync(Drive);
+        }
+    }
+
+    [RelayCommand]
+    private async Task UpdateIpAddressAsync()
+    {
+        string result = await PromptUserAsync("Edit IP Address?", "What's your new IP Address?");
+
+        if (string.IsNullOrWhiteSpace(result) is false)
+        {
+            Drive.IpAddress = result;
+            await _driveService.UpdateDriveAsync(Drive);
+        }
+    }
+
+    [RelayCommand]
+    private async Task UpdatePasswordAsync()
+    {
+        string result = await PromptUserAsync("Edit Password?", "What's your new Password?");
+
+        if (string.IsNullOrWhiteSpace(result) is false)
+        {
+            Drive.Password = result;
+            await _driveService.UpdateDriveAsync(Drive);
+        }
+    }
+
+    [RelayCommand]
+    private async Task UpdateUserNameAsync()
+    {
+        string result = await PromptUserAsync("Edit Username?", "What's your new Username?");
+
+        if (string.IsNullOrWhiteSpace(result) is false)
+        {
+            Drive.UserName = result;
+            await _driveService.UpdateDriveAsync(Drive);
+        }
     }
 }
