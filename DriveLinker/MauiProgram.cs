f7644b1f4aebb78bea4f60b5fe3edcbc4ed3407d
Timothy Tandt <Timothy.tandt@outlook.be>
2023-07-23 14:36:38 +0200

    Implemented Tektur Font

--- DriveLinker/MauiProgram.cs
+++ DriveLinker/MauiProgram.cs
@@ -13,6 +13,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+
+				fonts.AddFont("Tektur-Regular.ttf", "TekturRegular");
 			});
 
 #if DEBUG
